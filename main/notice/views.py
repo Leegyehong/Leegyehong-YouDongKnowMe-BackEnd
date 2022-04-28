ec1563c1fa5018741d3407b0639653111c63579e
ChanYoung <rlacksdud3@naver.com>
2022-04-28 17:13:01 +0900

    fix views.py 학과 없는 버전

--- main/notice/views.py
+++ main/notice/views.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from dataclasses import fields
 from re import L
 from django.shortcuts import render,get_list_or_404
@@ -11,6 +12,7 @@ import json
 # Create your views here.
 
 class NoticeList(View):
+    """
     def get(self, requset, majorname):
         try:
             notice = Test.objects.filter(major = majorname)
@@ -21,13 +23,36 @@ class NoticeList(View):
                 return HttpResponse(data,content_type="text/json-comment-filtered")
         except Exception as e:
             raise Http404('게시글을 찾을 수 없습니다 다시 요청해주세요!')  
-        
+    """
+    def get(self, requset):
+        try:
+            notice = Test.objects.all()
+            if notice.count() == 0:
+                raise Exception() 
+            else :
+                data = serializers.serialize("json",list(notice),fields=('num','title','writer'))
+                return HttpResponse(data,content_type="text/json-comment-filtered")
+        except Exception as e:
+            raise Http404('게시글을 찾을 수 없습니다 다시 요청해주세요!')  
     
 class NoticeDetail(View):
-    def get(self, requset, noticenum, major):
-        noticedetail = get_list_or_404(Test,num = noticenum, major = major)
-        data = serializers.serialize("json",noticedetail,fields = ('num','title','writer','info','filename','fileurl'))   
+    def get(self, requset, noticenum):
+        noticedetail = get_list_or_404(Test,num = noticenum)
+        data = serializers.serialize("json",noticedetail,fields = ('num','title','writer','content','file_url','date'))   
         temp = json.loads(data)
-        temp[0]['fields']['filename'] = temp[0]['fields']['filename'].split()
+        print(temp);
+        if(temp[0]['fields']['file_url'] != None):
+            temp[0]['fields']['file_url'] = temp[0]['fields']['file_url'].split()
         data = json.dumps(temp, indent=2, ensure_ascii=False)
         return HttpResponse(content=data)
+    """
+    class NoticeDetail(View):
+        def get(self, requset, noticenum, major):
+        noticedetail = get_list_or_404(Test,num = noticenum, major = major)
+        data = serializers.serialize("json",noticedetail,fields = ('num','title','writer','content','file_url','date'))   
+        temp = json.loads(data)
+        if(temp[0]['fields']['file_url'] != None):
+            temp[0]['fields']['file_url'] = temp[0]['fields']['file_url'].split()
+        data = json.dumps(temp, indent=2, ensure_ascii=False)
+        return HttpResponse(content=data)
+    """
