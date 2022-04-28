490c0cfa92cb2acd21c890b83a9a93fa65fa18da
ChanYoung <rlacksdud3@naver.com>
2022-04-28 17:14:25 +0900

    fix models.py 컬럼명 변경

--- main/notice/models.py
+++ main/notice/models.py
@@ -1,3 +1,10 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
@@ -116,13 +123,13 @@ class DjangoSession(models.Model):
 
 
 class Test(models.Model):
-    num = models.BigIntegerField(blank=True, null=True)
-    major = models.TextField(blank=True, null=True)
+    num = models.BigIntegerField()
+    id = models.BigIntegerField(primary_key=True)
     title = models.TextField(blank=True, null=True)
     writer = models.TextField(blank=True, null=True)
-    id = models.BigIntegerField(primary_key=True)
-    test = models.TextField(blank=True, null=True)
-    filename = models.CharField(max_length=1000, blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    file_url = models.TextField(blank=True, null=True)
 
     class Meta:
         managed = False
