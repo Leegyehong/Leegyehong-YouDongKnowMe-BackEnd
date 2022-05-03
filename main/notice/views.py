0cecde525636ee0c94f8844c5c2546660b6e3edd
rlacksdud3 <rlacksdud3@naver.com>
2022-05-03 12:59:14 +0900

    add : 검색 추가 urls.py views.py

--- main/notice/views.py
+++ main/notice/views.py
@@ -1,13 +1,12 @@
 from dataclasses import fields
+from logging import exception
+from multiprocessing.sharedctypes import Value
 from re import L
-from django.shortcuts import render, get_list_or_404
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse,JsonResponse,Http404
 from django.views import View
 from .models import Noti
 from django.core import serializers
-from django.http import Http404
 from http import HTTPStatus
-
 import json
 
 # Create your views here.
@@ -15,20 +14,25 @@ import json
 
 class NoticeList(View):
     """
-    def get(self, requset, majorname):
-        try:
-            notice = Noti.objects.filter(major = majorname)
+    def get(self, requset, major):
+         try:
+            notice = Noti.objects.using('crawled_data').filter(major = major).order_by('-num')
             if notice.count() == 0:
-                raise Exception() 
-            else :
-                data = serializers.serialize("json",list(notice),fields=('num','title','writer'))
-                return HttpResponse(data,content_type="text/json-comment-filtered")
+                raise Exception()
+            else:
+                data = serializers.serialize("json", list(
+                    notice), fields=('num', 'title','date', 'writer'))
+                temp = json.loads(data)
+                datalist = []  
+                for i in range(len(temp)):
+                    datalist.append(temp[i]['fields'])
+                data = json.dumps(datalist, indent=2, ensure_ascii=False)
+                return HttpResponse(data, content_type="text/json-comment-filtered")
         except Exception as e:
-            raise Http404('게시글을 찾을 수 없습니다 다시 요청해주세요!')  
+            return JsonResponse({'message':'error'},status=HTTPStatus.BAD_REQUEST)
     """
 
     def get(self, requset):
-        pass
         try:
             notice = Noti.objects.using('crawled_data').all().order_by('-num')
             if notice.count() == 0:
@@ -36,7 +40,12 @@ class NoticeList(View):
             else:
                 data = serializers.serialize("json", list(
                     notice), fields=('num', 'title','date', 'writer'))
-                return HttpResponse(data, content_type="text/json-comment-filtered")
+                temp = json.loads(data)
+                datalist = []  
+                for i in range(len(temp)):
+                    datalist.append(temp[i]['fields'])
+                data = json.dumps(datalist, indent=2, ensure_ascii=False)
+                return HttpResponse(data, content_type="application/json")
         except Exception as e:
             return JsonResponse({'message':'error'},status=HTTPStatus.BAD_REQUEST)
             #raise JS('게시글을 찾을 수 없습니다 다시 요청해주세요!')
@@ -45,38 +54,67 @@ class NoticeList(View):
 class NoticeDetail(View):
 
     def get(self, requset, noticenum):
-        noticedetail = Noti.objects.using('crawled_data').filter(num=noticenum).order_by('-num')
-        data = serializers.serialize("json", noticedetail, fields=(
-            'num', 'title', 'writer', 'content', 'file_url', 'date', 'img_url'))
-        temp = json.loads(data)
-        print(temp)
-        if(temp[0]['fields']['file_url'] != None):
-            temp[0]['fields']['file_url'] = temp[0]['fields']['file_url'].split()
-        if(temp[0]['fields']['img_url'] != None):
-            temp[0]['fields']['img_url'] = temp[0]['fields']['img_url'].split()
+        try:
+            noticedetail = Noti.objects.using('crawled_data').filter(num = noticenum).order_by('-num')
+            data = serializers.serialize("json", noticedetail, fields=(
+                'num', 'title', 'writer', 'content', 'file_url', 'date', 'img_url'))
             
-        # 메서드화 필요
-        if not temp[0]['fields']['content']:
-            temp[0]['fields']['content'] = ""
-        temp[0]['fields']['content']=temp[0]['fields']['content'].replace(" ", "")
+            temp = json.loads(data)
+            
+            for i in temp[0]['fields']:
+                if(temp[0]['fields'][i] == None or temp[0]['fields'][i].replace(" ", "") == ""):
+                    temp[0]['fields'][i] = ""
+                
+            if(temp[0]['fields']['file_url'] != ""):
+                temp[0]['fields']['file_url'] = temp[0]['fields']['file_url'].split()
+            if(temp[0]['fields']['img_url'] != ""):
+                temp[0]['fields']['img_url'] = temp[0]['fields']['img_url'].split()
+                
+            data = json.dumps(temp[0]['fields'], indent=2, ensure_ascii=False)
+            return HttpResponse(content=data)
         
-        if not temp[0]['fields']['file_url']:
-            temp[0]['fields']['file_url'] = ""
+        except Exception as e:
+            return JsonResponse({'message':'error'},status=HTTPStatus.BAD_REQUEST)
+   
+"""
+   class NoticeDetail(View):
+    
+    def get(self, requset, noticenum, major):
+        try:
+            noticedetail = Test.objects.using('crawled_data').filter(num = noticenum, major = major).order_by('-num')
+            data = serializers.serialize("json", noticedetail, fields=(
+                'num', 'title', 'writer', 'content', 'file_url', 'date', 'img_url'))
+            temp = json.loads(data)
+            for i in temp[0]['fields']:
+                if(temp[0]['fields'][i] == None or temp[0]['fields'][i].replace(" ", "") == ""):
+                    temp[0]['fields'][i] = ""
+                
+            if(temp[0]['fields']['file_url'] != ""):
+                temp[0]['fields']['file_url'] = temp[0]['fields']['file_url'].split()
+            if(temp[0]['fields']['img_url'] != ""):
+                temp[0]['fields']['img_url'] = temp[0]['fields']['img_url'].split()
+                
+            data = json.dumps(temp, indent=2, ensure_ascii=False)
+            return HttpResponse(content=data)
         
-        
-        if not temp[0]['fields']['img_url']:
-            temp[0]['fields']['img_url'] = ""
-        
-        data = json.dumps(temp, indent=2, ensure_ascii=False)
-        return HttpResponse(content=data)
-    """
-    class NoticeDetail(View):
-        def get(self, requset, noticenum, major):
-        noticedetail = get_list_or_404(Noti,num = noticenum, major = major)
-        data = serializers.serialize("json",noticedetail,fields = ('num','title','writer','content','file_url','date','img'))   
-        temp = json.loads(data)
-        if(temp[0]['fields']['file_url'] != None):
-            temp[0]['fields']['file_url'] = temp[0]['fields']['file_url'].split()
-        data = json.dumps(temp, indent=2, ensure_ascii=False)
-        return HttpResponse(content=data)
-    """
+        except Exception as e:
+            return JsonResponse({'message':'error'},status=HTTPStatus.BAD_REQUEST)
+"""
+
+class NoticeSearch(View):  
+    def get(self, requset, search, major):
+        try :
+            searchList = Noti.objects.using('crawled_data').filter(title__contains = search, major = major).order_by('-num')
+            if searchList.count() == 0:
+                raise Exception()
+            else:
+                data = serializers.serialize("json", list(
+                    searchList), fields=('num', 'title','date', 'writer'))
+                temp = json.loads(data)
+                datalist = []  
+                for i in range(len(temp)):
+                    datalist.append(temp[i]['fields'])
+                data = json.dumps(datalist, indent=2, ensure_ascii=False)
+                return HttpResponse(data, content_type="application/json")
+        except Exception as e:
+            return JsonResponse({'message':'error'},status=HTTPStatus.BAD_REQUEST)
