0cb3cac8d165ee13c9144a1234396ed9a81b8041
Leegyehong <59252624+Leegyehong@users.noreply.github.com>
2022-05-07 16:09:00 +0900

    fix : 필요없는 파일 제거

--- main/apikey/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_views
-
-app_name='apikey'
-
-urlpatterns = [
-    path('', views.index, name='index')
-]
