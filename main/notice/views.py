5c166390a2a201a760484a699aff2b2e3f5bd6b4
rlacksdud3 <rlacksdud3@naver.com>
2022-05-09 23:57:47 +0900

    fix : json 데이터 형식 변경

--- main/notice/views.py
+++ main/notice/views.py
@@ -29,24 +29,6 @@ class NoticeList(View):
                 return HttpResponse(data, content_type="application/json")
         except Exception as e:
             return JsonResponse({'message':str(e)},status=HTTPStatus.BAD_REQUEST)
-    """
-    def get(self, request):
-        try:
-            notice = Noti.objects.using('crawled_data').all().order_by('-num')
-            if notice.count() == 0:
-                raise Exception()
-            else:
-                data = serializers.serialize("json", list(
-                    notice), fields=('num', 'title','date', 'writer'))
-                temp = json.loads(data)
-                datalist = []  
-                for i in range(len(temp)):
-                    datalist.append(temp[i]['fields'])
-                data = json.dumps(datalist, indent=2, ensure_ascii=False)
-                return HttpResponse(data, content_type="application/json")
-        except Exception as e:
-            return JsonResponse({'message':str(e)},status=HTTPStatus.BAD_REQUEST)
-    """
 
 class NoticeDetail(View):
 
@@ -58,65 +40,24 @@ class NoticeDetail(View):
                 'num', 'title', 'writer', 'content', 'file_url', 'date', 'img_url'))
             
             temp = json.loads(data)
-            
             for i in temp[0]['fields']:
                 if(type(temp[0]['fields'][i]) != int):
                     if(temp[0]['fields'][i] == None or temp[0]['fields'][i].replace(" ", "") == ""):
                         temp[0]['fields'][i] = []
-                
-            if(temp[0]['fields']['file_url'] != []):
-                temp[0]['fields']['file_url'] = temp[0]['fields']['file_url'].split()
+            file_data = json.loads(temp[0]['fields']['file_url'].replace("'","\"")) 
+            temp[0]['fields']['file_url'] = []
+            for i in range(len(file_data)):
+              temp[0]['fields']['file_url'] += file_data[i].items()
             if(temp[0]['fields']['img_url'] != []):
                 temp[0]['fields']['img_url'] = temp[0]['fields']['img_url'].split()
+            
             data = json.dumps(temp[0]['fields'], indent=2, ensure_ascii=False)
             return HttpResponse(content=data)
         except Exception as e:
             return JsonResponse({'message':str(e)},status=HTTPStatus.BAD_REQUEST)
         
 
-"""
-   class NoticeDetail(View):
-    
-    def get(self, request, noticenum, major):
-        try:
-            noticedetail = Noti.objects.using('crawled_data').filter(num = noticenum, major = major).order_by('-num')
-            data = serializers.serialize("json", noticedetail, fields=(
-                'num', 'title', 'writer', 'content', 'file_url', 'date', 'img_url'))
-            temp = json.loads(data)
-            for i in temp[0]['fields']:
-                if(temp[0]['fields'][i] == None or temp[0]['fields'][i].replace(" ", "") == ""):
-                    temp[0]['fields'][i] = ""
-                
-            if(temp[0]['fields']['file_url'] != ""):
-                temp[0]['fields']['file_url'] = temp[0]['fields']['file_url'].split()
-            if(temp[0]['fields']['img_url'] != ""):
-                temp[0]['fields']['img_url'] = temp[0]['fields']['img_url'].split()
-                
-            data = json.dumps(temp, indent=2, ensure_ascii=False)
-            return HttpResponse(content=data)
-        
-        except Exception as e:
-            return JsonResponse({'message':'error'},status=HTTPStatus.BAD_REQUEST)
-"""
-
 class NoticeSearch(View):
-    """
-    def get(self, request):
-        try:
-            keyword = request.GET['keyword']
-            searchList = Noti.objects.using('crawled_data').filter(
-                title__contains=keyword).order_by('-num')     
-            data = serializers.serialize("json", list(
-                searchList), fields=('num', 'title', 'date', 'writer'))
-            temp = json.loads(data)
-            datalist = []
-            for i in range(len(temp)):
-                datalist.append(temp[i]['fields'])
-            data = json.dumps(datalist, indent=2, ensure_ascii=False)
-            return HttpResponse(data, content_type="application/json")
-        except Exception as e:
-            return JsonResponse({'message': str(e)}, status=HTTPStatus.BAD_REQUEST)
-    """
     def get(self, request, major):
         try:
             keyword = request.GET['keyword']
