25432002ff7c25421a0ce9ed5ca7fabecbac65b1
Leegyehong <59252624+Leegyehong@users.noreply.github.com>
2022-06-06 22:51:22 +0900

    add : messaging

--- dmu-crawling/test.py
+++ dmu-crawling/test.py
@@ -7,22 +7,27 @@ firebase_admin.initialize_app(cred)
 
 def send_to_firebase_cloud_messaging():
     # This registration token comes from the client FCM SDKs.
-    #registration_token = 'dV_frwRzSB21yx6pFOtZWS:APA91bHCv95X0UTWo-X7gF_uJ1NlJTBOy6UDNris5bDKwbLChZ1B8Ccv-8gKyj4x2-OVwdduBz-yz0uHCMzETnuO2kEKH_xzZa_spynCh1ttBMWaHx7ajfF5O-brxuXz-p_Wv3ES0jDj'
-    topic = 'weather'
+    #registration_token = 'cdqgXE8IRJ2mkhbo9Qld2x:APA91bF3aQMyO87ZthR92VfOsdpg5ITKIUh9wOalHXl1SwfrtwCWAqOdOrrOxxUqgFDSQ5wSm0bk5kkO8NgKod1Iyi5SE14PXqFPAvcvisJphSKA8stZ-7cEpwUaHitXW2yadVOD8W8e'
+    topic = 'corona'
 
     # See documentation on defining a message payload.
     message = messaging.Message(
-        notification=messaging.Notification(
-            title='김정호 스타좁밥',
-            body='테사기 뺴애애애액',
-        ),
-        # token=registration_token,
+        data={
+            'major_code': '1',
+            'num': '120',
+            'title' :'[코로나19] 확진자 발생 및 검사시행 안내',
+            'topic' : topic
+        },
+        # notification=messaging.Notification(
+        #     title = '등록하신 키워드 알림이 도착했습니다'
+        # ),
+        #token=registration_token,
         topic = topic,
     )
 
     response = messaging.send(message)
     # Response is a message ID string.
     print('Successfully sent message:', response)
-    
+
 
 send_to_firebase_cloud_messaging()
