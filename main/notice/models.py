30207a8cadb39808cae412ccb7f67ee003f25a9a
Leegyehong <khong0819@naver.com>
2022-06-13 00:02:25 +0900

    add : data

---
+++ main/notice/models.py
@@ -0,0 +1,35 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Noti(models.Model):
+    major_code = models.IntegerField(primary_key=True)
+    num = models.IntegerField()
+    title = models.TextField(blank=True, null=True)
+    writer = models.TextField(blank=True, null=True)
+    date = models.TextField(blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    img_url = models.TextField(blank=True, null=True)
+    file_url = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'noti'
+        unique_together = (('major_code', 'num'),)
+
+
+class Schedule(models.Model):
+    year = models.BigIntegerField(blank=True, null=True)
+    month = models.BigIntegerField(blank=True, null=True)
+    date = models.TextField(blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'schedule'
