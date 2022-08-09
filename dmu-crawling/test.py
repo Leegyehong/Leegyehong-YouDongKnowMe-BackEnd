35947bf278ea3176897e5f50a0d989c8fd3e10c1
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-08-09 20:55:45 +0900

    fix : messaging

--- dmu-crawling/test.py
+++ dmu-crawling/test.py
@@ -8,18 +8,16 @@ firebase_admin.initialize_app(cred)
 def send_to_firebase_cloud_messaging():
     # This registration token comes from the client FCM SDKs.
     #registration_token = 'cdqgXE8IRJ2mkhbo9Qld2x:APA91bF3aQMyO87ZthR92VfOsdpg5ITKIUh9wOalHXl1SwfrtwCWAqOdOrrOxxUqgFDSQ5wSm0bk5kkO8NgKod1Iyi5SE14PXqFPAvcvisJphSKA8stZ-7cEpwUaHitXW2yadVOD8W8e'
-    topic = 'corona'
-
-    # See documentation on defining a message payload.
+    topic = 'exam'
+    major_code = '1'
+    
     message = messaging.Message(
         data={
-            'major_code': '1',
-            'num': '120',
-            'title' :'[코로나19] 확진자 발생 및 검사시행 안내',
-            'topic' : topic
+            'major_code': major_code,
+            'num': '3597',
+            'title' :'전자출결 푸시(PUSH) 서비스 운영에 따른 전자출결 앱 업데이트 안내',
+            'keyword' : topic
         },
-        
-        #token=registration_token,
         topic = topic,
     )
 
